acbd07c55e20c79be3be84f953a3ec81242600bf
Corey Alexander <coreyja@gmail.com>
2017-12-09 18:25:45 -0500

    8a

---
+++ 2017/8-registers/a/solve.rb
@@ -0,0 +1,23 @@
+regex = /^(?<register>\w+) (?<direction>\w+) (?<value>-?[\d]+) if (?<con_register>\w+) (?<con_operator>[<>=!]+) (?<con_value>-?\d+)$/
+input = File.read('./input.txt')
+
+
+matches = []
+input.scan(regex){ matches << $~ }
+
+registers = Hash.new(0)
+
+matches.each do |instruction|
+	if eval("#{registers[instruction[:con_register]]} #{instruction[:con_operator]} #{instruction[:con_value]}")
+		case instruction[:direction]
+		when 'inc'
+			registers[instruction[:register]] += instruction[:value].to_i
+		when 'dec'
+			registers[instruction[:register]] -= instruction[:value].to_i
+		end
+	end
+end
+
+p registers
+
+p registers.values.max
