f3f01d9f5412aa4a734e3f2728712e2a090db41d
Corey Alexander <coreyja@gmail.com>
2018-12-08 00:25:50 -0500

    5b way faster

--- 2018/5/5b/solution.rb
+++ 2018/5/5b/solution.rb
@@ -4,26 +4,24 @@ input = File.read("#{__dir__}/../input.txt").strip
 
 puts 'Starting Length', input.length, ''
 
-def remove_first_opposite_unit(array)
-  diff = (0...array.count - 1).each_cons(2).find do |first_i, second_i|
-    first = array[first_i]
-    second = array[second_i]
+def remove_opposite_units(input)
+  uniq_units = input.downcase.chars.uniq
 
-    first.casecmp(second).zero? &&
-      first != second
+  current = input
+  uniq_units.each do |unit|
+    current = current.gsub(/#{unit}#{unit.upcase}/, '')
+    current = current.gsub(/#{unit.upcase}#{unit}/, '')
   end
-
-  2.times { array.delete_at(diff.first) } if diff
-  array
+  current
 end
 
 def make_as_short_as_possible(input)
   current_input = input
 
   loop do
-    starting_length = current_input.count
-    current_input = remove_first_opposite_unit(current_input)
-    ending_length = current_input&.count
+    starting_length = current_input.length
+    current_input = remove_opposite_units(current_input)
+    ending_length = current_input.length
 
     break if starting_length == ending_length
   end
@@ -40,12 +38,9 @@ foo = {}
 uniq_units.each do |unit|
   new_input = input.gsub(/[#{unit}#{unit.upcase}]/,'')
   p "Starting #{unit}"
-  foo[unit] = make_as_short_as_possible(new_input.chars)
+  foo[unit] = make_as_short_as_possible(new_input)
 end
 
-bar = foo.map { |k, v| [k, v.count] }
+bar = foo.map { |k, v| [k, v.length] }
 
-puts bar
-
-puts(bar.min_by { |key, value| value.count })
-
+puts(bar.min_by { |key, value| value })
